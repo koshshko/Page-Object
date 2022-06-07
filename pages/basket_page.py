89d445c366db53a8d015c12d7bd0c44d83269677
Anna <75202228+koshshko@users.noreply.github.com>
2022-06-07 22:32:32 +0300

    Update basket_page.py

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -2,16 +2,9 @@ from .base_page import BasePage
 from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
-    def empty_basket_tests(self):
-        self.basket_should_be_empty()
-        self.basket_is_empty_message()
-
     def basket_should_be_empty(self):
-        assert self.is_not_element_present(*BasketPageLocators.PRODUCTS_IN_BASKET), "Basket might have products added"
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Cart doesn't empty"
 
-    def basket_is_empty_message(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_TEXT), "Basket might not be empty"
-
-    def should_be_authorized_user(self):
-        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
-                                                                     " probably unauthorised user"
+    def should_be_message_that_basket_empty(self):
+        basket_empty_txt = self.browser.find_element(*BasketPageLocators.BASKET_EMPTY_TXT).text
+        assert "Your basket is empty" in basket_empty_txt, "Basket doesn't contains message 'Your basket is empty'"
