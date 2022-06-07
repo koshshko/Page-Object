c8b8db23c581d3d283c4f2e518827eaeb1960e85
Anna <75202228+koshshko@users.noreply.github.com>
2022-06-07 21:17:41 +0300

    Add files via upload

---
+++ pages/pages/base_page.py
@@ -0,0 +1,51 @@
+import math
+from selenium.common.exceptions import TimeoutException, NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def solve_quiz_and_get_code(self):  #метод в тесте для получения проверочного кода
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        WebDriverWait(self, browser, 5).until(EC.alert_is_present)
+        alert.accept()
+        WebDriverWait(self, browser, 5).until(EC.alert_is_present)
+        #try:
+            #alert = self.browser.switch_to.alert
+            #alert_text = alert.text
+            #print(f"Your code: {alert_text}")
+            #alert.accept()
+        #except NoAlertPresentException:
+            #print("No second alert presented")
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+    
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
