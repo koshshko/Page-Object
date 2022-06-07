4f9da89097c0336ce24096c844a770de92403aed
Anna <75202228+koshshko@users.noreply.github.com>
2022-06-07 22:25:26 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,13 +8,18 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url.find("login") != -1, "Login link is incorrect"
+        assert "login" in self.browser.current_url, "Login url isn't presented"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASS).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASS).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT_BTN).click()
 
 
